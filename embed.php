69c21600ea9da8d052fc142f61c96009cbcdfe7b
delbyabreu <51720572+delbyabreu@users.noreply.github.com>
2020-07-06 23:42:24 -0400

    Update embed.php

--- embed.php
+++ embed.php
@@ -24,7 +24,7 @@ if($_GET['url'] != ""){
 
 	<div id="myElement"></div>
 
-	<script src="https://github.com/tnserver/tnserver.github.io"></script>
+	<script src="https://raw.githubusercontent.com/tnserver/tnserver.github.io/master/jwplayer/0P4vdmeO.js"></script>
 	<script type="text/javascript">
 		jwplayer("myElement").setup({
 			playlist: [{
